80378ef59bf6b8fbff87b026955a1481db08b52b
tetsu5555 <tetete1118@gmail.com>
2019-10-27 16:23:21 +0900

    study: loadbalancing options

--- server/server.js
+++ server/server.js
@@ -9,7 +9,11 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send(`Hello world from server ${PORT}`);
+    res.send(`Hello world from server ${PORT}\n`);
+});
+
+app.get('/health_check', (req, res) => {
+    res.status(200).send();
 });
 
 app.listen(PORT, HOST);
